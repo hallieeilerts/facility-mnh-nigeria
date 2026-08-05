ae85f857556078e8658651025dffc05076224e4f
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2026-08-05 15:39:21 -0400

    aug 5

--- make.R
+++ make.R
@@ -10,11 +10,9 @@ source("./src/set-up/pull-data.R", local = new.env())
 
 # Monitoring --------------------------------------------------------------
 
-#source("./src/monitoring/monitoring-report-toolA.Rmd", local = new.env())
-#source("./src/monitoring/monitoring-table-toolA.Rmd", local = new.env())
-
 source("./src/monitoring/clean-toolC.R", local = new.env())
 rmarkdown::render("./src/monitoring/monitoring-table-toolC.Rmd")
+rmarkdown::render("./src/monitoring/monitoring-toolC.Rmd") # this one combines, pull-data, clean-toolC, and making the report
 
 source("./src/monitoring/clean-toolC-q27.R", local = new.env())
 source("./src/monitoring/clean-toolC-MaternPeriDeathRepeat.R", local = new.env())
@@ -24,9 +22,4 @@ source("./src/monitoring/monitoring-report-data-collection.Rmd", local = new.env
 
 
 
-# key off of f03 instead of the KEY
-# real server
-# https://odkc.akenahealth.org
-# Email: mmary1@jhu.edu
-# Password: marypassword
 
